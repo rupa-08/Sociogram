d8ae2470c051b330510f9230d3837b2a973b5b7f
“rupa-09” <rupa.shrestha@frostdigi.ai>
2023-11-11 09:29:36 +0545

    feat: signup form created.

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,15 @@
+import * as z from "zod";
+
+export const SignUpValidation = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name should be more than one character." }),
+  username: z
+    .string()
+    .min(2, { message: "User name must be more than one character." })
+    .max(50),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters." }),
+});
