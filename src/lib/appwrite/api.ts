6190a40f362caa0065fb72018f743dbbb0756527
“rupa-09” <rupa.shrestha@frostdigi.ai>
2023-11-18 21:25:36 +0545

    feat: post card design created and render in homepage done.

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -146,9 +146,9 @@ export async function uploadFile(file: File) {
   }
 }
 
-export async function getFilePreview(fileId: string) {
+export function getFilePreview(fileId: string) {
   try {
-    const fileUrl = await storage.getFilePreview(
+    const fileUrl = storage.getFilePreview(
       appwriteConfig.storageId,
       fileId,
       2000,
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -26,7 +26,7 @@ export function formatDateString(dateString: string) {
 }
 
 //
-export const multiFormatDateString = (timestamp: string = ""): string => {
+export const formatDate = (timestamp: string = ""): string => {
   const timestampNum = Math.round(new Date(timestamp).getTime() / 1000);
   const date: Date = new Date(timestampNum * 1000);
   const now: Date = new Date();
@@ -45,7 +45,7 @@ export const multiFormatDateString = (timestamp: string = ""): string => {
     case Math.floor(diffInDays) > 1 && diffInDays < 30:
       return `${Math.floor(diffInDays)} days ago`;
     case Math.floor(diffInHours) >= 1:
-      return `${Math.floor(diffInHours)} hours ago`;
+      return `${Math.floor(diffInHours)} hour ago`;
     case Math.floor(diffInMinutes) >= 1:
       return `${Math.floor(diffInMinutes)} minutes ago`;
     default:
