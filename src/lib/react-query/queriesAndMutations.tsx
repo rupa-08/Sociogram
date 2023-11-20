5a3a5a1392ec6382a9999e9bb7ba82dbee0ea723
“rupa-09” <rupa.shrestha@frostdigi.ai>
2023-11-20 16:10:13 +0545

    feat: post stats completed, like and save post completed.

--- src/lib/react-query/queriesAndMutations.tsx
+++ src/lib/react-query/queriesAndMutations.tsx
@@ -9,6 +9,7 @@ import {
   createPost,
   createUserAccount,
   deleteSavedPost,
+  getCurrentUser,
   getRecentPosts,
   likePost,
   savePost,
@@ -130,5 +131,12 @@ export const useDeleteSavedPost = () => {
         queryKey: [QUERY_KEYS.GET_CURRENT_USER],
       });
     },
+  });
+};
+
+export const useGetCurrentUser = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+    queryFn: getCurrentUser,
   });
 };
