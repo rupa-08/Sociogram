a1a092415e0a3f32f706f861766d9efccfa0cb91
“rupa-09” <rupa.shrestha@frostdigi.ai>
2023-11-25 12:14:10 +0545

    feat: post crud operation done.

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -16,7 +16,11 @@ import { Button } from "../ui/button";
 import FileUploader from "../shared/FileUploader";
 import { postValidation } from "@/lib/validation";
 import { Models } from "appwrite";
-import { useCreatePost } from "@/lib/react-query/queriesAndMutations";
+import {
+  useCreatePost,
+  useDeletePost,
+  useUpdatePost,
+} from "@/lib/react-query/queriesAndMutations";
 import { useUserContext } from "@/context/AuthContext";
 import { useToast } from "../ui/use-toast";
 import { useNavigate } from "react-router-dom";
@@ -27,11 +31,17 @@ type PostFormProps = {
 };
 
 const PostForm = ({ post, action }: PostFormProps) => {
-  const { user } = useUserContext();
-  const { toast } = useToast();
+  const navigate = useNavigate();
+
   const { mutateAsync: createPost, isPending: isLoadingCreate } =
     useCreatePost();
-  const navigate = useNavigate();
+  const { mutateAsync: updatePost, isPending: isUpdatingPost } =
+    useUpdatePost();
+  const { mutateAsync: deletePost, isPending: isDeletingPost } =
+    useDeletePost();
+
+  const { user } = useUserContext();
+  const { toast } = useToast();
 
   const form = useForm<z.infer<typeof postValidation>>({
     resolver: zodResolver(postValidation),
@@ -44,6 +54,20 @@ const PostForm = ({ post, action }: PostFormProps) => {
   });
 
   const handleFormSubmit = async (values: z.infer<typeof postValidation>) => {
+    if (post && action === "Update") {
+      const updatedPost = await updatePost({
+        ...values,
+        postId: post?.$id,
+        imageId: post?.imageId,
+        imageUrl: post?.imageUrl,
+      });
+
+      if (!updatedPost) {
+        toast({ title: "Please try again." });
+      }
+      return navigate(`/posts/${post.$id}`);
+    }
+
     const newPost = await createPost({ ...values, userId: user.id });
 
     if (!newPost) {
@@ -136,8 +160,10 @@ const PostForm = ({ post, action }: PostFormProps) => {
           <Button
             type="submit"
             className="shad-button_primary whitespace-nowrap"
+            disabled={isLoadingCreate || isUpdatingPost}
           >
-            Submit
+            {isLoadingCreate || (isUpdatingPost && "Loading...")}
+            {action} Post
           </Button>
         </div>
       </form>
